e4d99b8b77b5682546828d93582d7987ca01df1a
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-12-09 21:35:50 +0100

    Lesson1

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+
+            }
+        }
+    }
+}
