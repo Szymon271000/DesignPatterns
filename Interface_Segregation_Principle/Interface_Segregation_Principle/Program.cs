1ef15b75ca8bbfb6728d0de984b9e737df0e1b35
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-12-11 10:30:05 +0100

    Interface_Segregation_Principle

---
+++ Interface_Segregation_Principle/Interface_Segregation_Principle/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace Interface_Segregation_Principle
+{
+    class Program
+    {
+        public class Document
+        {
+
+        }
+
+        public interface IMachine
+        {
+            void Print(Document d);
+            void Scan(Document d);
+            void Fax(Document d);
+        }
+
+        public class MultiFunctionPrinter : IMachine
+        {
+            public void Fax(Document d)
+            {
+                //
+            }
+
+            public void Print(Document d)
+            {
+                //
+            }
+
+            public void Scan(Document d)
+            {
+                //
+            }
+        }
+
+        public class OldFashionedPrinter : IMachine
+        {
+            public void Fax(Document d)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Print(Document d)
+            {
+                //
+            }
+
+            public void Scan(Document d)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public interface IPrinter
+        {
+            void Printer(Document d);
+        }
+
+        public interface IScanner
+        {
+            void Scan(Document d);
+        }
+
+        public class Photocopier : IPrinter, IScanner
+        {
+            public void Printer(Document d)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Scan(Document d)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public interface IMultiFunctionDevice : IScanner, IPrinter ///
+        {
+
+        }
+
+        public class MultiFunctionMachine : IMultiFunctionDevice
+        {
+            private IPrinter printer;
+            private IScanner scanner;
+
+            public MultiFunctionMachine(IPrinter printer, IScanner scanner)
+            {
+                this.printer = printer;
+                this.scanner = scanner;
+            }
+
+            public void Printer(Document d)
+            {
+                printer.Printer(d);
+            }
+
+            public void Scan (Document d)
+            {
+                scanner.Scan(d);
+            } // decorator
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
