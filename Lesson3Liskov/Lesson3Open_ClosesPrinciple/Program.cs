18dc345e1a5134135d65c550f3178ed3fae02936
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-12-11 10:18:54 +0100

    Liskov_Substitution

---
+++ Lesson3Liskov/Lesson3Open_ClosesPrinciple/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Lesson3Open_ClosesPrinciple
+{
+    class Program
+    {
+        public class Rectangle
+        {
+            public Rectangle(int width, int height)
+            {
+                Width = width;
+                Height = height;
+            }
+
+            public Rectangle()
+            {
+
+            }
+            public virtual int Width { get; set; }
+            public virtual int Height { get; set; }
+
+            public override string ToString()
+            {
+                return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+            }
+        }
+        public class Square : Rectangle
+        {
+            public override int Width
+            {
+                set
+                {
+                    base.Width = base.Height = value;
+                }
+            }
+
+            public override int Height
+            {
+                set
+                {
+                    base.Width = base.Height = value;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            static int Area(Rectangle r) 
+            {
+                int area= r.Width* r.Height;
+                return area;
+            }
+
+            Rectangle rc = new Rectangle(2,3);
+            Console.WriteLine($"{rc} has area {Area(rc)}");
+
+            Square sq = new Square();
+            sq.Width = 4;
+            Console.WriteLine($"{sq} has area {Area(sq)}");
+        }
+    }
+}
