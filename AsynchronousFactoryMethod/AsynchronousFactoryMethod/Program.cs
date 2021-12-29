c2e8d751de1a249adabf623a6510e926dc153ea1
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-12-29 11:17:31 +0100

    AsynchronousFactoryMethod

---
+++ AsynchronousFactoryMethod/AsynchronousFactoryMethod/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsynchronousFactoryMethod
+{
+    class Program
+    {
+        public class Foo
+        {
+            private Foo()
+            {
+                //
+            }
+
+            private async Task<Foo> InitAsync()
+            {
+                await Task.Delay(1000);
+                return this;
+            }
+
+            public static Task<Foo> CreateAsyn()
+            {
+                var result = new Foo();
+                return result.InitAsync();
+            }
+        }
+        
+        public class Demo
+        {
+            public static async Task Main (string[] args)
+            {
+                Foo x = await Foo.CreateAsyn();
+            }
+        }
+    }
+}
