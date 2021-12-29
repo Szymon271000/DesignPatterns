704eb0621a7e0803b0551df3563e2ddd7692a1ec
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-12-29 11:48:24 +0100

    Factory2ObjectTrackingAndBulkReplacement

--- Factory/Factory/Program.cs
+++ Factory/Factory/Program.cs
@@ -9,6 +9,19 @@ namespace Factory
             Cartesian,
             Polar
         }
+
+        public static class PointFactory
+        {
+            public static Point NewCartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+
+            public static Point NewPolarPoint(double rho, double theta)
+            {
+                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+            }
+        }
         public class Point
         {
             private double x, y;
@@ -20,16 +33,8 @@ namespace Factory
             /// <param name="system"></param>
             /// 
             //factory method
-            public static Point NewCartesianPoint(double x, double y)
-            {
-                return new Point(x, y);
-            }
-
-            public static Point NewPolarPoint(double rho, double theta)
-            {
-                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
-            }
-            private Point(double x, double y)
+            
+            public Point(double x, double y)
             {
                 this.x = x;
                 this.y = y;
@@ -55,7 +60,8 @@ namespace Factory
         }
         static void Main(string[] args)
         {
-            var point = Point.NewPolarPoint(1.0, Math.PI / 2);
+            
+            var point = PointFactory.NewPolarPoint(1.0, Math.PI / 2);
             Console.WriteLine(point);
         }
     }
