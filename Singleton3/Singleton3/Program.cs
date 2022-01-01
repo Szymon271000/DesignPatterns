bb10276dba8d23595302a83e6e588493ddb45427
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-01-01 18:03:31 +0100

    Singleton3

---
+++ Singleton3/Singleton3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Singleton3
+{
+    public sealed class PerThreadSingleton
+    {
+        private static ThreadLocal<PerThreadSingleton> threadInstance = new ThreadLocal<PerThreadSingleton>(() => new PerThreadSingleton());
+
+        public int id;
+        private PerThreadSingleton()
+        {
+            id = Thread.CurrentThread.ManagedThreadId;
+        }
+        public static PerThreadSingleton Instance => threadInstance.Value;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t1 = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine($"t1: {PerThreadSingleton.Instance.id}");
+            });
+
+            var t2 = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine($"t2: {PerThreadSingleton.Instance.id}");
+                Console.WriteLine($"t2: {PerThreadSingleton.Instance.id}");
+
+            });
+            Task.WaitAll(t1, t2);
+        }
+    }
+}
