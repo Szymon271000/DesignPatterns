e76c764d21ac6335ef1cf12236e36a3d98ba2b87
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-01-01 17:57:06 +0100

    Singleton2/Monostate

---
+++ Monostate/Monostate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Monostate
+{
+    class Program
+    {
+        public class CEO
+        {
+            private static string name;
+            private static int age;
+
+            public string Name { get => name; set => name = value; }
+            public int Age { get => age; set => age = value; }
+
+            public override string ToString()
+            {
+                return $"{nameof(name)}: {name}, {nameof(age)}: {age}";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var ceo = new CEO();
+            ceo.Name = "Adam Smith";
+            ceo.Age = 55;
+
+            var ceo2 = new CEO();
+            Console.WriteLine(ceo2);
+        }
+    }
+}
