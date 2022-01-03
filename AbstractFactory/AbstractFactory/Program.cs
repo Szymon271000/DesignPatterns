e44d2bef9b23630aedefd608307f8be083ff6857
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-01-03 14:23:27 +0100

    AbstractFactory

--- AbstractFactory/AbstractFactory/Program.cs
+++ AbstractFactory/AbstractFactory/Program.cs
@@ -1,9 +1,79 @@
 ﻿using System;
+using System.Data;
+using System.Data.Common;
 
 namespace AbstractFactory
 {
     class Program
     {
+        interface DatabaseAbstractFactory
+        {
+            DbConnection CreateConnection(string connString);
+            DbCommand CreateCommand(IDbConnection connection, string sql);
+        }
+
+        interface DBConnection
+        {
+            string ConnectionString { get; set; }
+            void Open();
+            void Close();
+        }
+
+        interface DbCommand
+        {
+            object Execute();
+        }
+
+        public class SQLServerFactory : DatabaseAbstractFactory
+        {
+            public DbCommand CreateCommand(IDbConnection connection, string sql)
+            {
+                return new SqlServerCommand(connection, sql);
+            }
+
+            public DbConnection CreateConnection(string connString)
+            {
+                return new SqlServerConnection(connString);
+            }
+        }
+
+        class SqlServerConnection : DBConnection
+        {
+            private string ConnectionString { get; set; }
+
+            public SqlServerConnection(string str) {
+                ConnectionString = str;
+            }
+
+            public void Close()
+            {
+                Console.WriteLine("Close connection");
+            }
+
+            public void Open()
+            {
+                Console.WriteLine($"Open connection: {ConnectionString}");
+            }
+
+            class SqlServerCommand : DbCommand
+            {
+                private DbConnection connection { get; set; }
+                private string sql { get; set; }
+
+                public SqlServerCommand(DbConnection conn, string sql)
+                {
+                    this.connection = conn;
+                    this.sql = sql;
+                }
+                public object Execute()
+                {
+                    connection.Open();
+                    Console.WriteLine("Esecuzione: " + sql);
+                    connection.Close();
+                    return null;
+                }
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
